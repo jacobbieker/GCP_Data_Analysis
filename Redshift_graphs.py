dfde4d3b461b74b7702a0ba818d6815ce8f4784c
Jacob Bieker <jacob@jacobbieker.com>
2015-04-20 22:42:13 -0700

    Updated CSV file, removing rows that wer just spaces Created working graph

--- Redshift_graphs.py
+++ Redshift_graphs.py
@@ -4,24 +4,22 @@ import math
 import os
 import csv
 import numpy as np
-import matplotlib as pyplot
+import matplotlib.pyplot as pyplot
+import matplotlib.mlab as mlab
 
 data = 'GCP_spectroscopicdata.csv'
 bin_size = 0.1
 redshift_array = []
-bins = np.arange(0, 1.2, bin_size)
+bins = np.arange(0.0, 2.1, bin_size)
 
 f = open(data)
 csv_file = csv.reader(f)
 next(csv_file)
 for line in csv_file:
-    print float(line[3])
+    print line[0]
+    print line[3]
     redshift_array.append(float(line[3]))
 
 
-pyplot.hist(redshift_array, bins=0.1)
-hist, edges = np.histogram(redshift_array, bins=0.1)
-width = 0.7 * (edges[1] - edges[0])
-center = (edges[:-1] + edges[1:])/2
-pyplot.bar(center, hist, align='center', width=width)
+pyplot.hist(redshift_array, bins)
 pyplot.show()
