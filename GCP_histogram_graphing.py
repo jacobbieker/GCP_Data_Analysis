64bbcc197651cb6acc3172c30270e3d5d69a39fa
Jacob BIeker <jacob@jacobbieker.com>
2015-04-22 11:27:20 -0700

    Add min_bin and max_bin to change start and end of bin size depending on the lower and upper bounds of the data

--- GCP_histogram_graphing.py
+++ GCP_histogram_graphing.py
@@ -29,7 +29,7 @@ e_l_h_beta_em = 25 #error in l_h_beta_em
 l_fe = 26 #log10(Fe)=log((Fe5270+F5335)/2) index fully corrected
 e_l_fe = 27 #error in l_fe
 
-
+ 
 def create_histogram(name_for_graph, data, column_to_graph, bin_size):
     max_bin = 0.0
     min_bin = 0.0
