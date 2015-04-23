b7f249183a2402f54f211b85a85208ab71470f1c
Jacob BIeker <jacob@jacobbieker.com>
2015-04-23 13:41:32 -0700

    Created GCP_lin_indicies.py for the line index graphs

--- GCP_histogram_graphing.py
+++ GCP_histogram_graphing.py
@@ -1,5 +1,5 @@
 from __future__ import division
-__author__ = 'Jacob'
+__author__ = 'Jacob Bieker'
 import csv
 import numpy as np
 import matplotlib.pyplot as pyplot
---
+++ GCP_line_indicies.py
@@ -0,0 +1,5 @@
+from __future__ import division
+__author__ = 'Jacob Bieker'
+import numpy as np
+import matplotlib.pyplot as pyplot
+import csv
